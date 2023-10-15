6c5a8a8064569e7597faf94a6c5fe6b540ef447f
Shivam-2113 <86293101+Shivam-2113@users.noreply.github.com>
2023-10-16 00:52:04 +0530

    Add files via upload

---
+++ Java_Assignment/Pangram.java
@@ -0,0 +1,34 @@
+package Java_Assignment;
+
+//Check if the input is pangram or not. (A pangram is a sentence that contains all the
+//alphabets from A to Z)
+
+import java.util.Scanner;
+
+public class Pangram {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        if (isPangram(str)) {
+            System.out.println("Pangram");
+        } else {
+            System.out.println("Not Pangram");
+        }
+    }
+
+    public static boolean isPangram(String str) {
+        str = str.toLowerCase();
+        boolean flag = true;
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (!str.contains(String.valueOf(ch))) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            return true;
+
+        return false;
+    }
+}
